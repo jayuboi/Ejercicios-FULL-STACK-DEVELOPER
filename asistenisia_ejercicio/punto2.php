4641f53ee36613d9163fdd573812c87edd30d127
Kevin Felipe Alvarado Carranza <86944966+jayuboi@users.noreply.github.com>
2023-04-14 16:29:52 -0500

    commit inicial

---
+++ asistenisia_ejercicio/punto2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="punto2.php" method="post">
+    <input type="number" name="correctas" placeholder="Preguntas correctas">
+    <input type="number" name="incorrectas" placeholder="Preguntas incorrectas">
+    <input type="number" name="blanco" placeholder="Preguntas en blanco">
+    <input type="submit" name="operador" value="validar">
+
+
+</form>
+
+</body>
+</html>
+<?php
+$correctas = null;
+$incorrectas = null;
+$blanco = null;
+$total = null;
+
+if (condition) 
+#code
+?>
