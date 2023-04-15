09ebd38424ddee0fb693519b75ba72812a4a51b5
kevin <17kevinfelipe17@gmail.com>
2023-04-14 20:31:21 -0500

    ejercicios unidad 3

---
+++ Unidad_3/ejercicio3.php
@@ -0,0 +1,15 @@
+<?php
+
+include "ejercico3.html";
+
+$cantidad = null;
+$i =0;
+
+if(isset($_POST["numero"])){
+
+    for($i=1$i<=$cantidad;$i++){
+        echo "Hola mundo $i <br>"
+
+//que es una iteracion
+
+}
